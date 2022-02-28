7707e7aca444a59e4c84ef15be9658c7c4c80101
IraklyPapava <93033063+IraklyPapava@users.noreply.github.com>
2022-02-28 17:26:55 +0300

    Update PZ_13_2.py

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,1 +1,12 @@
+# Составить генератор (yield), который переведет символы строки из нижнего
+# регистра в верхний.
+
+#stroke = input('Введите строку в нижнем регистре: ')
+stroke = 'qwerty'
+def up(str):
+    yield str.upper()
+
+new_stroke = up(stroke)
+for i in new_stroke:
+    print(i)
 
