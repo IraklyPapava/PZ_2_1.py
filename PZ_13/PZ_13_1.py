17ff0b97fc9a475b1ee2a2e3bd938fd186109201
IraklyPapava <93033063+IraklyPapava@users.noreply.github.com>
2022-02-28 17:26:29 +0300

    Update PZ_13_1.py

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,1 +1,10 @@
+#В последовательности на n целых чисел умножить все элементы на первый
+#элемент.
+#from random import randint
+#dlina = int(input('Введите длину последовательности: '))
+#n = [randint(-10, 10) for i in range(dlina)]
 
+n = [3, 2, 3, 5, 6]
+print(n)
+nn = [i*n[0] for i in n]
+print(nn)
